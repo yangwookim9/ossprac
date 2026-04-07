5eb07eb332669ea43fe7fc00f462c9005245fe60
yangwookim9 <2022110481@dgu.ac.kr>
2026-04-07 09:13:31 +0900

    2025-04-07 개인과제5 4번

--- subject5/main.py
+++ subject5/main.py
@@ -1,7 +1,9 @@
 name = input("이름을 입력하세요: ")
 student_id = input("학번을 입력하세요: ")
+department = input("학과를 입력하세요: ")
 
 print()
 print("<출력>")
 print("이름:", name)
 print("학번:", student_id)
+print("학과:", department)
