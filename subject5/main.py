133c5eeb4af6b1d5de73411120dcd9883a0917ee
yangwookim9 <2022110481@dgu.ac.kr>
2026-04-14 20:29:38 +0900

    modify main.py for assignment

--- subject5/main.py
+++ subject5/main.py
@@ -1,8 +1,12 @@
-#개인과제5
+#개인과제6
 name = input("이름을 입력하세요: ")
 student_id = input("학번을 입력하세요: ")
+department = input("학과를 입력하세요: ")
+school = input("학교를 입력하세요: ")
 
 print()
 print("<출력>")
 print("이름:", name)
 print("학번:", student_id)
+print("학과:", department)
+print("학교:", school)
